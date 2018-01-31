edc5e3035e262d5f936979e34ac2516d43d0e92a
Simone D <monatru3@hotmail.com>
2018-01-31 02:25:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,10 +19,8 @@ function whileLoop(n) {
 
 
 
-function doWhileLoop(array, function maybeTrue() {
-  return Math.random() >= 0.5
-}) {
+function doWhileLoop(array) {
   do {
-    
-    } while(maybeTrue()||array.length===0);
+    array.pop()
+  } while(maybeTrue()&&array.length===0);
 };
